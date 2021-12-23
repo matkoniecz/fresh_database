844ed04b66009c1c21fd8f1006efca90f9eb0bbd
Mateusz Konieczny <matkoniecz@gmail.com>
2021-12-23 12:59:28 +0100

    new round of improvements to setting up analysis

--- load_all_changeset_data_line_by_line.py
+++ load_all_changeset_data_line_by_line.py
@@ -1,16 +1,20 @@
-import requests
+#import requests
+import
 
-response = requests.get("https://www.delftstack.com/howto/python/python-get-json-from-url/")
-
-print(response)
-print(response.text)
+# changeset data can be obtained with https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp
 
 row_count = 0
 # changeset_id,created_by,creation_date,changed_objects,user_id
-with open('edits_with_all_declared_software.csv') as fp:
+os.chdir("~/Desktop/sc_statistics_service_development/sc-statistics-service") # make it more generic
+with open('/OSMcache/edits_with_all_declared_software.csv') as fp:
     for line in fp:
         if row_count != 0:
             if "StreetComplete" in line.split(",")[1]:
                 print(line)
-                print(line.split(",")[-1])
+                user_id = line.split(",")[-1]
+                print(user_id)
+                os.system("php update_users.php " + user_id)
+                #url = "127.0.0.1:8000/get_statistics.php?user_id=" + user_id
+            	#response = requests.get(url)
+                #print(response.text)
         row_count += 1
