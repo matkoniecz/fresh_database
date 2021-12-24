274832e752f0de4429d9e42b722bde419ca7aeea
Mateusz Konieczny <matkoniecz@gmail.com>
2021-12-24 11:25:48 +0100

    futureproofing script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
+sudo apt-get install -y php mariadb-server php-mysql php-curl php-xml git
 cd ..
 git clone https://github.com/streetcomplete/sc-statistics-service.git
 cd sc-statistics-service
 cp  config.sample.php config.php # this setting file can be modified, not just blindly copied (in such case amend later lines)
 cd ..
-sudo apt-get install -y php mariadb-server php-mysql php-curl php-xml
 # ideally following would read from config.php rather than assume that it is unmodified
 sudo mysql -u root -e "CREATE DATABASE statistics;"
 sudo mysql -u root -e "CREATE USER statistics_user@localhost IDENTIFIED BY 'statistics_pw';"
