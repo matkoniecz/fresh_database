40baf8507cebeababd0ec4d8bcfd80698142c141
Mateusz Konieczny <matkoniecz@gmail.com>
2022-01-02 12:03:54 +0100

    properly specify file location

--- load_all_changeset_data_line_by_line.py
+++ load_all_changeset_data_line_by_line.py
@@ -1,12 +1,20 @@
 #import requests
-import
+#import
 
 # changeset data can be obtained with https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp
 
+if len(sys.argv) != 2:
+    print("call it with a single arguments specifying location of a csv file")
+    print("csv file must be made in format ANY_FIELD,created_by,ANY_FIELD,ANY_FIELD,user_id")
+    print("(for example by https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp )")
+
+# just start it there...
+#os.chdir("~/Desktop/sc_statistics_service_development/sc-statistics-service") # make it more generic
+
+file_location = sys.argv[1]
 row_count = 0
 # changeset_id,created_by,creation_date,changed_objects,user_id
-os.chdir("~/Desktop/sc_statistics_service_development/sc-statistics-service") # make it more generic
-with open('/OSMcache/edits_with_all_declared_software.csv') as fp:
+with open(file_location) as fp:
     for line in fp:
         if row_count != 0:
             if "StreetComplete" in line.split(",")[1]:
