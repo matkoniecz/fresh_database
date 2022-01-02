97dceb0919031379c66a528700f43d1f6148c7fe
Mateusz Konieczny <matkoniecz@gmail.com>
2022-01-02 12:05:49 +0100

    fix bugs in script

--- load_all_changeset_data_line_by_line.py
+++ load_all_changeset_data_line_by_line.py
@@ -1,28 +1,32 @@
 #import requests
-#import
+import sys
 
-# changeset data can be obtained with https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp
+def main():
+    # changeset data can be obtained with https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp
 
-if len(sys.argv) != 2:
-    print("call it with a single arguments specifying location of a csv file")
-    print("csv file must be made in format ANY_FIELD,created_by,ANY_FIELD,ANY_FIELD,user_id")
-    print("(for example by https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp )")
+    if len(sys.argv) != 2:
+        print("call it with a single arguments specifying location of a csv file")
+        print("csv file must be made in format ANY_FIELD,created_by,ANY_FIELD,ANY_FIELD,user_id")
+        print("(for example by https://github.com/matkoniecz/StreetComplete_usage_changeset_analysis#streetcomplete_edits_generate_csv_and_make_quest_summaryphp )")
+        return
 
-# just start it there...
-#os.chdir("~/Desktop/sc_statistics_service_development/sc-statistics-service") # make it more generic
+    # just start it there...
+    #os.chdir("~/Desktop/sc_statistics_service_development/sc-statistics-service") # make it more generic
 
-file_location = sys.argv[1]
-row_count = 0
-# changeset_id,created_by,creation_date,changed_objects,user_id
-with open(file_location) as fp:
-    for line in fp:
-        if row_count != 0:
-            if "StreetComplete" in line.split(",")[1]:
-                print(line)
-                user_id = line.split(",")[-1]
-                print(user_id)
-                os.system("php update_users.php " + user_id)
-                #url = "127.0.0.1:8000/get_statistics.php?user_id=" + user_id
-            	#response = requests.get(url)
-                #print(response.text)
-        row_count += 1
+    file_location = sys.argv[1]
+    row_count = 0
+    # changeset_id,created_by,creation_date,changed_objects,user_id
+    with open(file_location) as fp:
+        for line in fp:
+            if row_count != 0:
+                if "StreetComplete" in line.split(",")[1]:
+                    print(line)
+                    user_id = line.split(",")[-1]
+                    print(user_id)
+                    os.system("php update_users.php " + user_id)
+                    #url = "127.0.0.1:8000/get_statistics.php?user_id=" + user_id
+                    #response = requests.get(url)
+                    #print(response.text)
+            row_count += 1
+
+main()
